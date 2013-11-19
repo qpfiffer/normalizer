d9e0097396cbe4bccddc3d2097de40036e506775
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-11-18 18:56:16 -0800

    Stubs

--- normalize.hs
+++ normalize.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Aeson
 import Data.Text as T
 import Data.Text.IO as TIO
 import System.Environment
@@ -8,14 +9,18 @@ import System.Exit
 data Nick = Nick
     { nick :: Text
     , alias_of :: Text
-    }
+    } deriving Show
 
---parseLog :: Text -> 
+instance ToJSON Nick where
+    toJSON (Nick name alias) = object [ "nick" .= name, "alias_of" .= alias ]
+
+parseLog :: Text -> [Nick]
+parseLog _ = []
 
 main :: IO ()
 main = do
     args <- getArgs
     file_data <- TIO.readFile (args !! 0)
-    --parseLog file_data
+    return $ parseLog file_data
 
     exitWith ExitSuccess
